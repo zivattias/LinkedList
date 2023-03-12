64b83032ad05f6519ece78bd524347ea48085278
zivattias <ziv.attias7@gmail.com>
2023-03-12 10:37:18 +0200

    implemented iterator protocol for LinkedList(), properties & setters and len(), append(), insert(). Also modified LinkedList.__init__ to accept by-iterable initialization

--- linkedlist.py
+++ linkedlist.py
@@ -1,51 +1,129 @@
-# LinkedList() consits multiple Node()s that map to each other.
-
 # 'annotations' enables class typing within its own class
 from __future__ import annotations
-from typing import Optional
+
+# advanced typing - Optional[Type] = Type | None, Iterable = iterator protocol implemented
+from typing import Optional, Iterable
 
 
 class Node:
-    def __init__(self, value=None):
-        self._value = value
-        self.__next: Optional[Node] = None
-        self.__previous: Optional[Node] = None
-
-    def __str__(self) -> str:
-        return f"<Node: {self._value}>"
+    def __init__(
+        self,
+        value=None,
+        next_node: Optional[Node] = None,
+        previous_node: Optional[Node] = None,
+    ):
+        self.__value = value
+        self.__next = next_node
+        self.__previous = previous_node
 
     @property
     def next(self) -> Optional[Node]:
         return self.__next
 
+    @next.setter
+    def next(self, node: Node):
+        self.next = node
+
     @property
     def previous(self) -> Optional[Node]:
         return self.__previous
 
+    @previous.setter
+    def previous(self, node: Node):
+        self.previous = node
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        self.value = value
+
 
 class LinkedList:
-    def __init__(self, iterable: Optional[LinkedList] = None) -> None:
-        if iterable is None:
-            self.__head: Optional[Node] = None
-            self.__tail: Optional[Node] = self._head
-            self.__len: int = 0
-        elif isinstance(iterable, LinkedList):
-            self._head, self._tail, self._len = (
-                iterable.head,
-                iterable.tail,
-                iterable.len,
-            )
-        else:
-            raise TypeError()
+    def __init__(self, iterable: Optional[Iterable] = None):
+        self.__head: Optional[Node] = None
+        self.__tail: Optional[Node] = None
+        self.__len: int = 0
+
+        if iterable is not None:
+            for item in iterable:
+                self.append(item)
 
     @property
     def head(self) -> Optional[Node]:
         return self.__head
 
+    @head.setter
+    def head(self, node: Node):
+        self.head = node
+
     @property
     def tail(self) -> Optional[Node]:
         return self.__tail
 
+    @tail.setter
+    def tail(self, node: Node):
+        self.tail = node
+
     @property
     def len(self) -> int:
         return self.__len
+
+    @len.setter
+    def len(self, value: int):
+        self.len = value
+
+    # Iterator protocol implementation:
+    def __iter__(self):
+        self.current_node = self.head
+        return self
+
+    def __next__(self):
+        if self.current_node is None:
+            return StopIteration
+
+        value = self.current_node.value
+        self.current_node = self.current_node.next
+        return value
+
+    # Iterator length:
+    def __len__(self):
+        return self.len
+
+    # Append implementation:
+    def append(self, value):
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+        else:
+            if self.tail is None:
+                self.head.next = new_node
+                new_node.previous = self.head
+            else:
+                self.tail.next = new_node
+                new_node.previous = self.tail
+
+        self.tail = new_node
+        self.len += 1
+
+    # Insert value at a specific index:
+    def insert(self, value, index: int):
+        if index < 0 or index > len(self):
+            raise IndexError("Insertion index out of range")
+
+        new_node = Node(value)
+
+        if index == 0:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            current_node = self.head
+            for _ in range(index - 1):
+                current_node = current_node.next
+            new_node.next = current_node.next
+            new_node.previous = current_node
+            current_node.next = new_node
+            current_node.next.previous = new_node
